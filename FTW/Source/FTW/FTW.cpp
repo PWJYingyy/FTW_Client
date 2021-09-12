63937ee0bab67e0de6ed099d53737a438be2e9c8
jaypan <jaypan@huanle.com>
2021-09-12 22:18:36 +0800

    创建工程

---
+++ FTW/Source/FTW/FTW.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FTW.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FTW, "FTW" );
---
+++ FTW/Source/FTW/FTWGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FTWGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FTW_API AFTWGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
